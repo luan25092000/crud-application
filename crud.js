09e2916e39b3098d8d173b5519ebff59e3395f35
luan25092000 <nguyenhuuluan17@gmail.com>
2023-01-07 14:21:08 +0700

    store and delete data localStorage

--- crud.js
+++ crud.js
@@ -13,7 +13,31 @@ $(document).ready(function () {
     // Regular expression check validate number
     function isNumeric(value) {
         return /^-?\d+$/.test(value);
-    }    
+    }  
+    
+    // Show data localStorage, JSON.parse chuyển chuỗi thành mảng lại
+    if (localStorage.getItem('studyTools') != '' && localStorage.getItem('studyTools') != null) { // Kiểm tra localStorage đã tồn tại chưa
+        var availableTools = JSON.parse(localStorage.getItem('studyTools'));
+        if (availableTools.length > 0) { // nếu như có data
+            for (let availableTool of availableTools) {
+                var formatPrice = availableTool.price.toLocaleString('it-IT', {style : 'currency', currency : 'VND'}); 
+                var formatSalePrice = availableTool.salePrice.toLocaleString('it-IT', {style : 'currency', currency : 'VND'});
+                var htmlAvailable = `
+                    <tr data-id=${availableTool.idProduct}>
+                        <td>${availableTool.idProduct}</td>
+                        <td>${availableTool.name}</td>
+                        <td>${formatPrice}</td>
+                        <td>${formatSalePrice}</td>
+                        <td>
+                            <i class="fa-solid fa-trash text-danger mr-2 btn-remove" style="cursor: pointer;"></i>
+                            <i class="fa-solid fa-pen text-primary" style="cursor: pointer;"></i>
+                        </td>
+                    </tr>
+                `;
+                $('#crudTable > tbody:last-child').append(htmlAvailable);
+            }
+        }
+    }
 
     $('#addBtn').click(function () {
         // Check required
@@ -77,10 +101,12 @@ $(document).ready(function () {
         }
         $('#sale-price-error').hide();
 
+        var idProduct = (Math.random() + 1).toString(36).substring(7);
         var formatPrice = price.toLocaleString('it-IT', {style : 'currency', currency : 'VND'}); 
         var formatSalePrice = salePrice.toLocaleString('it-IT', {style : 'currency', currency : 'VND'});
         var html = `
-            <tr>
+            <tr data-id=${idProduct}>
+                <td>${idProduct}</td>
                 <td>${name}</td>
                 <td>${formatPrice}</td>
                 <td>${formatSalePrice}</td>
@@ -92,6 +118,29 @@ $(document).ready(function () {
         `;
 
         $('#crudTable > tbody:last-child').append(html);
+
+        // Save data to localStorage
+        // Object
+        var studyTool = {
+            'idProduct': idProduct,
+            'name': name,
+            'price': price,
+            'salePrice': salePrice
+        };
+
+        // Th1: storage studyTools chưa có dữ liệu
+        if (localStorage.getItem('studyTools') == '' || localStorage.getItem('studyTools') == null) {
+            var studyTools = [];
+        // Th2: storage studyTools đã có dữ liệu
+        } else {
+            var studyTools = JSON.parse(localStorage.getItem('studyTools'));
+        }
+
+        studyTools.push(studyTool);
+
+        // Biến mảng dữ liệu thì chuỗi (string), vì localStorage chỉ có thể lữu chuỗi hoặc số
+        localStorage.setItem('studyTools', JSON.stringify(studyTools));
+
         $('#name').val('');
         $('#price').val('');
         $('#sale-price').val('');
@@ -99,6 +148,18 @@ $(document).ready(function () {
 
     // Delete record
     $('#crudTable').on('click', '.btn-remove', function() {
+        var dataId = $(this).parent().parent().data('id'); // data-id
+        if (localStorage.getItem('studyTools') != '' && localStorage.getItem('studyTools') != null) { // Kiểm tra localStorage đã tồn tại chưa
+            var availableTools = JSON.parse(localStorage.getItem('studyTools'));
+            if (availableTools.length > 0) { // nếu như có data
+                for (let key in availableTools) {
+                    if (dataId === availableTools[key].idProduct) { // nếu như dataId === idProduct nào đó trong localStorage
+                        availableTools.splice(key, 1); // Xóa đi phần tử đó trong localStorage
+                    }
+                }
+                localStorage.setItem('studyTools', JSON.stringify(availableTools));
+            }
+        }
         $(this).parent().parent().remove();
     });
 });
